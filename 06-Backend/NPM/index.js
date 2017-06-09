967e2fabd4f0cef4a501b7e1a24c557214f5f210
Tania Guamushig <tania.guamushig@epn.edu.ec>
2017-06-09 11:07:15 -0500

    all

---
+++ 06-Backend/NPM/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Tania Maricela on 05/06/2017.
+ */
+
+//index.js
+
+calculadoraUdla = {
+        sumar:(n1,n2)=>{
+        return n1+n2;
+},
+restar:(n1,n2)=>{
+    return n1-n2;
+},
+multiplicar:(n1,n2)=>{
+    return n1*n2;
+}
+};
